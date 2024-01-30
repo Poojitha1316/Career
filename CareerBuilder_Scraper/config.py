56612c310649b427cb39ac698227965102b92688
Poojitha1316 <84569735+Poojitha1316@users.noreply.github.com>
2024-01-30 13:02:52 +0530

    Add files via upload

---
+++ CareerBuilder_Scraper/config.py
@@ -0,0 +1,8 @@
+# config.py
+
+class Config:
+    URL_TEMPLATE = "https://www.careerbuilder.com/jobs?cb_apply=false&cb_workhome=all&emp=jtct%2Cjtc2%2Cjtcc&keywords={keyword}&location=&pay=&posted=&sort=date_desc&page={page}"
+    OUTPUT_FILENAME_TEMPLATE = "output.csv"
+    KEYWORD = "data analyst"
+    PROXY = "http://ef8d6c711db158d21b6d4ceea6114c518f6cf81f:@proxy.zenrows.com:8001"
+ 
